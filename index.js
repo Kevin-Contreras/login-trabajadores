7274bc6d44a22ad016505c7ae95575757f17a73d
Kevin <kevinacontrerasl62@gmail.com>
2020-06-02 07:24:44 -0600

    guardado

---
+++ index.js
@@ -0,0 +1,18 @@
+
+  var boton = document.querySelector("#boton1");
+  var textos1 = document.querySelector("#a");
+  var textos2 = document.querySelector("#b");
+  var textos3 = document.querySelector("#c");
+  var textos4 = document.querySelector("#d");
+  var textos5 = document.querySelector("#e");
+  var titulo = document.getElementById("titulo");
+
+  
+  
+  boton.addEventListener("click",function(e){
+textos1.value="";
+textos2.value="";
+textos3.value="";
+textos4.value="";
+textos5.value="";
+  })
